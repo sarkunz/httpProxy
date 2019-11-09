fea4f25d29244e83a0b1dc3ac8c82fe064f8c6ec
Sarah Mykelle Kunzler <sarkunz@illinois.cs.byu.edu>
2019-11-08 19:42:49 -0700

    requests and responses working, need to fix threading

--- proxy.c
+++ proxy.c
@@ -38,45 +38,14 @@ typedef struct{
 	http_header* next;
 } http_request;
 
-
-//DONT NEED? open_listenfd does this for me??
-// int init_socket(int port){
-//     printf("CREATE SOCKET");
-//     //create the socket
-//     sd = socket( AF_INET, SOCK_STREAM, 0 );
-//     if(sd < 0){
-//         printf("%s\n", "Your socket sux");
-//         return -1;
-//     }
-
-//     //set socket addr
-//     memset(&addr, 0, sizeof(addr));
-//     addr.sin_family = AF_INET;
-// 	addr.sin_port = htons((u_short) port);
-// 	addr.sin_addr.s_addr = INADDR_ANY;
-
-//     //bind the socket
-//     if (bind(sd, (struct sockaddr *) &addr, sizeof(addr)) < 0){
-// 		printf("%s\n", "Do you even bind bruh?");
-// 		return -1;
-// 	}
-
-//     //now we listen
-//     if( listen( sd, 100 ) == -1 ) {
-//         printf("%s\n", "Listen Linda");
-//         return -1;
-//     }
-
-//     //made it
-//     return 0;
-// }
-
 http_request* parseReq(char* buffer){
+    printf("PARSING REQ\n");
 	http_request* request = malloc(sizeof(http_request));
 	char* buffer_rest = buffer;
+    printf(&buffer);
 	char* line = strtok_r(strdup(buffer), "\r\n", &buffer_rest);
 	char* line_rest = line;
-
+    
 	char* type = strtok_r(strdup(line), " ", &line_rest);
 	request->type = type;//TODO need to malloc?
 
@@ -87,7 +56,7 @@ http_request* parseReq(char* buffer){
 	request->version = version;
 
 	http_header* current = NULL;
-
+    printf("here\n");
 	while (line = strtok_r(NULL, "\r\n", &buffer_rest)){
 		line_rest = line;
 
@@ -112,79 +81,212 @@ http_request* parseReq(char* buffer){
 	return request;
 }
 
+void add_header(http_request* request, char* name, char* value){
+	//TODO does it matter the order of headers?
+	http_header* new_header = malloc(sizeof(http_header));
+	new_header->name = name;
+	new_header->value = value;
+
+	http_header* next = request->next;
+	new_header->next = next;
+	request->next = new_header;
+}
+
+void remove_header(http_request* request, char* name){
+	http_header* past = NULL;
+	http_header* current = request->next;
+	while (current){
+		if (strcmp(current->name, name) == 0){
+			http_header* tmp = current;
+			current = current->next;
+			if (past){
+				past->next = current;
+			}
+			else{
+				request->next = current;
+			}
+			free(tmp);
+		}
+		else{
+			past = current;
+			current = current->next;
+		}
+	}
+}
+
+char* get_header(http_request* request, char* name){
+	http_header* current = request->next;
+	while (current){
+		if (strcmp(current->name, name) == 0){
+			return current->value;
+		}
+		current = current->next;
+	}
+	return NULL;
+}
+
+void replace_user_agent(http_request* request){
+	remove_header(request, "User-Agent");
+	add_header(request, "User-Agent", user_agent_hdr);
+}
+
+char* get_host_from_url(char* url){
+	char* url_rest = url;
+	char* host = strtok_r(url, "/", &url_rest);
+	printf("%s\n", "HOSTS");
+	printf("%s\n", url);
+	printf("%s\n", host);
+	printf("%s\n", url_rest);
+	return host;
+}
+
+char* get_content_from_url(char* url){
+	char* url_rest = url;
+	char* host = strtok_r(url, "//", &url_rest);
+	host = strtok_r(NULL, "/", &url_rest);
+	char* content = malloc(sizeof(url));
+	strcpy(content, "/");
+	strcat(content, url_rest);
+	return content;
+}
+
+void switchRequest(http_request* request){
+    request->version = http_version_hdr;
+    replace_user_agent(request);
+    remove_header(request, "Connection");
+    add_header(request, "Connection", "close");
+	remove_header(request, "Proxy-Connection");
+	add_header(request, "Proxy-Connection", "close");
+	if (!get_header(request, "Host")){
+		char* host = get_host_from_url(request->url);
+		add_header(request, "Host", host);
+	}
+	if (request->url[0] != '/'){
+		char* content = get_content_from_url(request->url);
+		request->url = content;
+	}
+}
+
+char* req_to_str(http_request* request){
+	//TODO need to malloc?
+	char* buffer = malloc(MAX_OBJECT_SIZE);
+	strcpy(buffer, request->type);
+	strcat(buffer, " ");
+	strcat(buffer, request->url);
+	strcat(buffer, " ");
+	strcat(buffer, request->version);
+	strcat(buffer, "\r\n");
+	http_header* current = request->next;
+	while(current){
+		strcat(buffer, current->name);
+		strcat(buffer, ":");//TODO add space?
+		strcat(buffer, current->value);
+		strcat(buffer, "\r\n");
+		current = current->next;
+	}
+	strcat(buffer, "\r\n");
+	return buffer;//TODO free
+}
+
+char *trimwhitespace(char *str){
+	char *end;
+
+	// Trim leading space
+	while(isspace((unsigned char)*str)) str++;
+
+	if(*str == 0)  // if it's all spaces
+		return str;
+
+	// Trim trailing space
+	end = str + strlen(str) - 1;
+	while(end > str && isspace((unsigned char)*end)) end--;
+
+	// Add new ending char
+	end[1] = '\0';
+	return str;
+}
+
+
+char* makeRequest(char* host, char* request){
+	char* host_rest = host;
+	char* server = strtok_r(host, ":", &host_rest);
+	char* port = NULL;
+	if (host_rest != NULL){
+		port = host_rest;
+	}
+	server = trimwhitespace(server);
+	port = trimwhitespace(port);
+	
+	rio_t rio;
+	int request_sock = Open_clientfd(server, port);
+	
+	char* response = malloc(MAX_OBJECT_SIZE);
+	memset(response, 0, sizeof(response));
+	Rio_readinitb(&rio, request_sock);
+	Rio_writen(request_sock, request, strlen(request));
+	//send(request_sock, request, strlen(request), 0);
+	char* response_line[MAXLINE];
+	int header_size = 0;
+	//Read headers
+	while (Rio_readlineb(&rio, response_line, MAXLINE) > 0){
+		if (strcmp(response_line, "\r\n") == 0){
+			break;
+		}
+		header_size += strlen(response_line);
+		//printf("{%s}", response_line);
+		strcat(response, response_line);
+	}
+	strcat(response, "\r\n");//TODO need this?
+	header_size += 2;
+	//printf("\nBEGIN<%s>END\n", response);
+
+	//Read body
+	char body[MAX_OBJECT_SIZE];
+	Rio_readnb(&rio, body, (MAX_OBJECT_SIZE - header_size));
+	memcpy(response+header_size, body, (MAX_OBJECT_SIZE - header_size));
+	//recv(request_sock, response, MAX_OBJECT_SIZE, 0);
+	
+	close(request_sock);
+
+	return response;
+}
 
 void *handle_connection( void *fdp){ //STUB. TODO finish
     printf("MADE THREAD\n");
     int fd = *((int *)fdp);
-    pthread_detach(pthread_self);
-    char buffer[MAX_LEN];
-    free(fdp);
+    Pthread_detach(Pthread_self());
+    Free(fdp);
     
-    printf("ZAI\n");
-
     ///////////copied code to read and write
-    //TODO this should be in thread
-    int total_bytes_read = 0, bytes_read;
-    do {
-        bytes_read = read( fdp, buffer + total_bytes_read, 4096 - total_bytes_read );
-
-        if( bytes_read == -1 ){
-        printf( "read error\n" );
-        exit( -1 );
-        }
-
-        total_bytes_read += bytes_read;
-    } while( total_bytes_read < 4 || !at_end( buffer + total_bytes_read - 4 ) ); 
-            // resource identifer has form /<number>+<number>
+    char* buffer = Malloc(MAX_OBJECT_SIZE);
+	recv(fd, buffer, MAX_OBJECT_SIZE, 0);
+	printf("Buffer %s\n", buffer);
 
     http_request* request = parseReq(buffer);
 	if (request == NULL){
 		printf("%s\n", "Invalid HTTP request");
-		close(fdp);
-		return;
+		close(fd);
+		exit(-1);
 	}
 
+    switchRequest(request);
 
-    // resource identifier starts 4 bytes in
-    // <number>+<number> starts 5 bytes in
-    char *resource = buffer + 5, *p = resource;
+    char* reqBuffer = req_to_str(request);
+    char* host = get_header(request, "Host");
+    char* response = makeRequest(host, reqBuffer); //TODO
+    free(reqBuffer);
 
-    while( *p != ' ' ) {
-        ++p;
-    }
+    //send response
+    Rio_writen(fd, response, MAX_OBJECT_SIZE);
 
-    *p = '\0';
-
-    printf("resoure: \n");
-    printf( "%s\n", resource );
-    printf("done");
-
-    int n, m;
-
-    sscanf( resource, "%d+%d", &n, &m );
-
-    sprintf( buffer, "HTTP/1.1 200 thumbsup\r\nContent-Encoding: text/plain\r\nContent-Length: %d\r\n\r\n%d", 2 , n + m );
-
-    int total_bytes_written = 0, bytes_to_write = strlen( buffer ), bytes_written;
-
-    do {
-        bytes_written = write( fdp, buffer + total_bytes_written, bytes_to_write - total_bytes_written );
-
-        if( bytes_written == -1 ) {
-        printf( "write error\n" );
-        exit( -1 );
-        }
-
-        total_bytes_written += bytes_written;
-    } while( total_bytes_written < bytes_to_write );
-
+    //free buffer
+    free(buffer);
     close(fd);
     return 0;
 }
 
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
     printf("Begin main\n");
     int listenfd, *connfdp;
     socklen_t clientlen;
